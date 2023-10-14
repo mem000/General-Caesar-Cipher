ee9a07e04fab9819b65cebe4f52774df32b88773
mahmo <mahmo@MEM-PC>
2023-10-14 04:05:51 +0300

    format code

--- src/temp_4_test/NativeOpenDialogDemo.java
+++ src/temp_4_test/NativeOpenDialogDemo.java
@@ -3,11 +3,7 @@ additional file for test purposes
  */
 package temp_4_test;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
